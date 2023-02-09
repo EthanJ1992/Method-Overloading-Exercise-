01a698e8a900817f4772225963e522450e316ba3
Ethan Jacobs <jacobethan1992@gmail.com>
2023-02-09 10:23:16 -0500

    method overload Ex1

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -6,5 +6,32 @@
         {
             Console.WriteLine("Hello, World!");
         }
+
+
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+
+        }
+        public static decimal Add(decimal num1, decimal num2)
+
+        {
+            return num1 + num2;
+        }
+
+        public static string Add(int num1, int num2, bool num3)
+        {
+            if (num3 == true)
+            {
+
+                return $"{num1 + num2} dollars";
+            }
+            else
+            {
+                return "Error";
+            }
+        }
     }
-}
+
+
+
